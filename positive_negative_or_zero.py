9a33d2b7c8df760ee6ef85610859e9c470e13ea9
Sakhayadeep <sakhayadeepnath@gmail.com>
2019-03-22 19:40:44 +0530

    positive negative or zero

---
+++ positive_negative_or_zero.py
@@ -0,0 +1,30 @@
+class Number:
+    def __init__(self, num = 0):
+        if(type(num) == type(int())):
+            self.num = num
+        else:
+            raise ValueError("Expected a int, {} given.".format(type(num)))
+
+    def set_number(self, num = 0):
+        if(type(num) == type(int())):
+            self.num = num
+        else:
+            raise ValueError("Expected a int, {} given.".format(type(num)))
+
+    def check_number(self):
+        if(self.num > 0):
+            return "Positive"
+        elif(self.num < 0):
+            return "Negative"
+        else:
+            return "Zero"
+
+def main():
+    my_num = Number()
+
+    my_num.set_number(int(input()))
+
+    print(my_num.check_number())
+
+if __name__ == "__main__":
+    main()
