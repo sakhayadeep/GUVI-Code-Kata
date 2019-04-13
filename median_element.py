9629fb6f308f27939e02f7bac5cb9ef25bed80d0
Sakhayadeep <sakhayadeepnath@gmail.com>
2019-04-14 00:18:15 +0530

    beginner set complete

---
+++ median_element.py
@@ -0,0 +1,5 @@
+n = input()
+x = list(map(int, input().split()))
+x.sort()
+i = len(x)//2
+print(x[i])
---
+++ print_element_with_index.py
@@ -0,0 +1,5 @@
+n = input()
+x = list(map(int, input().split()))
+
+for i,j in enumerate(x):
+    print(j, i)
